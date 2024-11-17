a01d06e6df52b72c813bc8db661578a73f30c594
01121922383osama <nasamt2002@gmail.com>
2024-11-17 12:13:31 +0200

    gg

--- server.js
+++ server.js
@@ -14,15 +14,21 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+// Basic route to test if server is running
+app.get('/', (_req, res) => {
+  res.send('WebRTC Chat Server is running');
+});
+
 // Store connected users and their socket IDs
 const users = new Map();
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected:', socket.id);
 
   // Handle user joining
   socket.on('join', (username) => {
     users.set(socket.id, username);
+    console.log(`${username} joined with socket ID: ${socket.id}`);
     io.emit('userJoined', username);
     io.emit('userList', Array.from(users.entries()).map(([id, name]) => ({
       id,
@@ -41,34 +47,68 @@ io.on('connection', (socket) => {
   });
 
   // WebRTC Signaling
-  socket.on('call-user', ({ userToCall, signalData, from }) => {
-    io.to(userToCall).emit('incoming-call', {
-      signal: signalData,
-      from,
-      username: users.get(from)
+  socket.on('call-user', (data) => {
+    console.log('Call request from', socket.id, 'to', data.userToCall);
+    io.to(data.userToCall).emit('incoming-call', {
+      signal: data.signalData,
+      from: socket.id,
+      username: users.get(socket.id)
     });
   });
 
-  socket.on('answer-call', ({ to, signal }) => {
-    io.to(to).emit('call-accepted', signal);
+  socket.on('answer-call', (data) => {
+    console.log('Call answered by', socket.id, 'to', data.to);
+    io.to(data.to).emit('call-accepted', data.signal);
   });
 
-  socket.on('end-call', ({ to }) => {
-    io.to(to).emit('call-ended');
+  socket.on('ice-candidate', (data) => {
+    console.log('ICE candidate from', socket.id, 'to', data.to);
+    io.to(data.to).emit('ice-candidate', {
+      candidate: data.candidate,
+      from: socket.id
+    });
+  });
+
+  // Handle track state changes
+  socket.on('track-ended', (data) => {
+    io.to(data.to).emit('track-ended', {
+      trackKind: data.trackKind
+    });
+  });
+
+  socket.on('track-muted', (data) => {
+    io.to(data.to).emit('track-muted', {
+      trackKind: data.trackKind
+    });
+  });
+
+  socket.on('track-unmuted', (data) => {
+    io.to(data.to).emit('track-unmuted', {
+      trackKind: data.trackKind
+    });
+  });
+
+  socket.on('end-call', (data) => {
+    console.log('Call ended by', socket.id, 'to', data.to);
+    io.to(data.to).emit('call-ended');
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
     const username = users.get(socket.id);
     users.delete(socket.id);
-    io.emit('userLeft', username);
-    io.emit('userList', Array.from(users.entries()).map(([id, name]) => ({
-      id,
-      username: name
-    })));
+    if (username) {
+      console.log(`${username} disconnected`);
+      io.emit('userLeft', username);
+      io.emit('userList', Array.from(users.entries()).map(([id, name]) => ({
+        id,
+        username: name
+      })));
+    }
   });
 });
 
+// Start the server
 http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
