549cd380fec9d0e4962a9cf759375d47091d0d98
01121922383osama <nasamt2002@gmail.com>
2024-11-19 13:21:20 +0200

    ok

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require('path');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +36,13 @@ app.post('/login', (req, res) => {
     res.redirect('/');
   }
 });
+
+// Handle logout
+app.get('/logout', (_req, res) => {
+  // Here, you can handle clearing any session or cookie data
+  res.redirect('/'); // Redirect to login page
+});
+
 
 // Store connected users and their socket IDs
 const users = new Map();
